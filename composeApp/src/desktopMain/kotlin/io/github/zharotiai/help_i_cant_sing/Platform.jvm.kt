9f82961317337e6ca3837af85a65ea164788b4ba
Ishaan Zharotia <iz424@ic.ac.uk>
2025-06-27 20:16:12 +0100

    removed desktop

--- composeApp/src/desktopMain/kotlin/io/github/zharotiai/help_i_cant_sing/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package io.github.zharotiai.help_i_cant_sing
-
-class JVMPlatform : Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
