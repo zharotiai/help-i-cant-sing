971fcaff2d8df28c1c76610b6b8c2c7ccf56d616
Ishaan Zharotia <iz424@ic.ac.uk>
2025-06-29 13:23:03 +0100

    Added AudioRecorder interface

--- composeApp/src/commonMain/kotlin/io/github/zharotiai/help_i_cant_sing/audio/AudioRecorder.kt
+++ composeApp/src/commonMain/kotlin/io/github/zharotiai/help_i_cant_sing/audio/AudioRecorder.kt
@@ -1,11 +1,16 @@
 package io.github.zharotiai.help_i_cant_sing.audio
 
+import kotlinx.coroutines.flow.Flow
+
+enum class RecordingState {
+    Idle, Recording, Paused
+}
+
 interface AudioRecorder {
     fun startRecording()
     fun pauseRecording()
     fun resumeRecording()
     fun stopRecording()
-    val isRecording: Boolean
-    val isPaused: Boolean
+    val state: Flow<RecordingState>
+    val audioBufferFlow: Flow<ShortArray> // Emits raw PCM audio buffers in real time
 }
-
